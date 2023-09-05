1abb3ee9e0438dc0deb7121e8fd0d13c2a5b0d0b
Noman73 <noman.eng73@gmail.com>
2023-09-05 12:26:16 +0600

    05-09-2023

--- app/Http/Controllers/Api/News/NewsController.php
+++ app/Http/Controllers/Api/News/NewsController.php
@@ -254,7 +254,7 @@ class NewsController extends Controller
         if($validator->passes()){
             $post=PostHasAuthor::with('details')->has('post',function($q)use($request){
                 $q->with('categories.category')->where('status','!=',Constant::POST_STATUS['deleted'])->orderBy('id','desc');
-            })->whereHas('post')->where('author_id',Auth::user()->id)->skip($request->offset)->take($request->limit)->get();
+            })->where('author_id',Auth::user()->id)->skip($request->offset)->take($request->limit)->get();
             return response()->json($post);
         }
         return response()->json(['status'=>false,'error'=>$validator->getMessageBag()]);

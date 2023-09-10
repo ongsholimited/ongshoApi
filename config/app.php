e267358b4fac606fd6ac6102cae760202114058d
Noman73 <noman.eng73@gmail.com>
2023-09-10 16:05:12 +0600

    10-09-2023

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
+    'proxies' => '*',
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
@@ -96,7 +96,7 @@ return [
     */
 
     'fallback_locale' => 'en',
-
+    
     /*
     |--------------------------------------------------------------------------
     | Faker Locale

cd7565c084c85f9c3223adb8809c062bdf71c336
Noman73 <noman.eng73@gmail.com>
2023-09-10 15:11:21 +0600

    10-09-2023

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -7,8 +7,18 @@ use Illuminate\Http\Request;
 use App\Models\News\HomeSection;
 class TestController extends Controller
 {
-    public function test()
+    public function test(Request $request)
     {
+      
+        $clientIp = $request->getClientIp();
+
+        return "Client's IP address: " . $clientIp;
+        
+        return "Client's Public IP Address: " . $ip;
+        $public_ip = file_get_contents('https://ipinfo.io/ip');
+        return  "Your public IP address is: $public_ip";
+  
+       
         return HomeSection::with('post')->get();
     }
 }

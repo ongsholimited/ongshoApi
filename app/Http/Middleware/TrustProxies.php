e8235d0dcdd1662db3b6e71f26810b4ca56c1f3d
Noman73 <noman.eng73@gmail.com>
2023-09-10 19:16:21 +0600

    10-09-2023

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-        protected $proxies;
+        protected $proxies=['X-Forwarded-For'];
 
     /**
      * The headers that should be used to detect proxies.

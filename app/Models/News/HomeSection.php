ce086da706b141816cc86a2efcfaea73c9c13a93
Noman73 <noman.eng73@gmail.com>
2023-09-12 19:46:12 +0600

    12-09-2023

--- app/Models/News/HomeSection.php
+++ app/Models/News/HomeSection.php
@@ -17,7 +17,7 @@ class HomeSection extends Model
     
     public function post()
     {
-        return $this->hasManyThrough(Post::class,PostHasCategory::class,'post_id','id','category_id','category_id');
+        return $this->hasManyThrough(Post::class,PostHasCategory::class,'category_id','id','category_id','category_id');
     }
     public function category(){
         return $this->belongsTo(Category::class,'category_id','id');

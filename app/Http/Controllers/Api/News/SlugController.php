13a8a39630542ab32fa57da16e1a3aa2042b776e
Noman73 <noman.eng73@gmail.com>
2023-09-19 14:02:22 +0600

    19-09-2023

--- app/Http/Controllers/Api/News/SlugController.php
+++ app/Http/Controllers/Api/News/SlugController.php
@@ -17,11 +17,8 @@ class SlugController extends Controller
     public function checkSlug($slug,$post_id=null)
     {
         // this condition for store and update option for frontend post
-        if($post_id==null){
-            $existance=SlugableTrait::slugCount($slug,$post_id);
-        }else{
-            $existance=SlugableTrait::slugCount($slug,$post_id);
-        }
+        
+        $existance=SlugableTrait::slugCount($slug,$post_id);
         if($existance>0){
             return SendDataApi::bind(['status'=>true,'count'=>$existance,'message'=>'the slug already exist']);
         }

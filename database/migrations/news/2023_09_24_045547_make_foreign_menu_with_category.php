ee5650bff9ba489d2c66634724cb63c1aed0bbca
Noman73 <noman.eng73@gmail.com>
2023-09-24 11:10:12 +0600

    24-09-2023

---
+++ database/migrations/news/2023_09_24_045547_make_foreign_menu_with_category.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('ongsho_news')->table('menus', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('ongsho_news')->table('menus', function (Blueprint $table) {
+            $table->dropForeign('menus_category_id_foreign');
+        });
+    }
+};

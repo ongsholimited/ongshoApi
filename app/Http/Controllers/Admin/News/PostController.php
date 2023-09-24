fb3a28f7f8680aa4ef4289314d80d7be51f5c21f
Noman73 <noman.eng73@gmail.com>
2023-09-24 13:33:44 +0600

    24-09-2023

--- app/Http/Controllers/Admin/News/PostController.php
+++ app/Http/Controllers/Admin/News/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     public function postList()
     {
       if(request()->ajax()){
-        $get=Post::with('categories','author.details')->get();
+        $get=Post::with('categories','author.details')->orderBy('id','desc')->get();
         return DataTables::of($get)
           ->addIndexColumn()
           ->addColumn('action',function($get){

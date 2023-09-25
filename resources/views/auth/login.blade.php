68b3bb04832ae785256a918a90f16fd9f4b757c0
Noman73 <noman.eng73@gmail.com>
2023-09-25 16:33:43 +0600

    25-09-2023

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="www.ongsho.com"><b>Ongsho </b></a>
+            <a href="url('/')"><b>Ongsho </b></a>
         </div>
 
         <div class="card">
